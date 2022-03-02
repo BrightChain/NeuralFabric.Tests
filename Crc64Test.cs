3929a10cf18025b689555ff17aad54904a3c53e0
Jessica Mulein <jessica@brightchain.org>
2022-03-01 20:31:49 -0800

    beefed up tests for crc32, new test for crc64

--- Crc64Test.cs
+++ Crc64Test.cs
@@ -7,19 +7,20 @@ using NeuralFabric.Helpers;
 namespace NeuralFabric.Tests;
 
 [TestClass]
-public class Crc32Test
+public class Crc64Test
 {
+    // TODO: verify CRC64 values!
     // ReSharper disable once StringLiteralTypo
     [DataTestMethod]
     [DataRow(data1: "54686520717569636B2062726F776E20666F78206A756D7073206F76657220746865206C617A7920646F67",
-        0x414FA339U)]
+        12758330164905647473U)]
     [DataRow(data1: "0000000000000000000000000000000000000000000000000000000000000000",
-        0x190A55ADU)]
+        0U)]
     [DataRow(data1: "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF",
-        0xFF6CAB0BU)]
+        651209950806147072U)]
     [DataRow(data1: "000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F",
-        0x91267E8AU)]
-    public void TestCrc32KnownHashes(string hexString, uint expected)
+        9260567444085364859U)]
+    public void TestCrc64KnownHashes(string hexString, ulong expected)
     {
         var data = new byte[hexString.Length / 2];
         for (int offset = 0, dataOffset = 0; offset < hexString.Length; offset += 2)
@@ -29,7 +30,7 @@ public class Crc32Test
                     length: 2),
                 fromBase: 16);
 
-        var actual = Crc32.ComputeChecksum(bytes: data);
+        var actual = Crc64.ComputeChecksum(bytes: data);
         Assert.AreEqual(
             expected: expected,
             actual: actual);
@@ -40,9 +41,9 @@ public class Crc32Test
     {
         var randomBytes = new Faker().Random.Bytes(count: 1000)!;
         // get the expected checksum using the uint version, which is tested more vigorously above
-        var expected = Crc32.ComputeChecksum(bytes: randomBytes);
+        var expected = Crc64.ComputeChecksum(bytes: randomBytes);
         var expectedBytes = BitConverter.GetBytes(value: expected);
-        var actual = Crc32.ComputeChecksumBytes(bytes: randomBytes);
+        var actual = Crc64.ComputeChecksumBytes(bytes: randomBytes);
         Assert.IsTrue(
             condition: expectedBytes.SequenceEqual(second: actual));
     }
@@ -50,7 +51,10 @@ public class Crc32Test
     [TestMethod]
     public void TestGeneratedTable()
     {
-        var generatedTable = Crc32.CreateTable(polynomial: Crc32.DefaultPolynomial);
-        Assert.IsTrue(condition: generatedTable.SequenceEqual(second: Crc32.DefaultTable));
+        var generatedTable = Crc64.CreateTable(polynomial: Crc64.Iso3309Polynomial);
+        Assert.IsTrue(condition: generatedTable.SequenceEqual(second: Crc64.Iso3309Table));
+
+        var generatedEcmaTable = Crc64.CreateTable(polynomial: Crc64.EcmaPolynomial);
+        Assert.IsTrue(condition: generatedEcmaTable.SequenceEqual(second: Crc64.EcmaTable));
     }
 }
